e5bfc954a6a14ba92db35e9e4500dcfcc6089a1c
DAM 1 <DAM 1@DESKTOP-TKRSI9T>
2024-01-16 09:32:38 +0100

    Creación de la Propiedad NoombreEmpleado

--- GestionNominasV1/Nomina.cs
+++ GestionNominasV1/Nomina.cs
@@ -16,6 +16,33 @@ namespace GestionNominasV1
         private int _horasTrabajadas;
         private float _salrioHora;
 
+        // Propiedades dar acceso a la interdaz, para dar protección
+
+        /// <summary>
+        /// Nombre del Empleado
+        /// </summary>
+        public string NombreEmpleado
+        {
+            get
+            {
+                // Control de Contenido
+                if (string.IsNullOrEmpty(_nombre)) throw new Exception("Nombre del trabajador no establecido");
+
+                // Devolución del Valor
+                return _nombre;
+            }
+            set
+            {
+                // Validación de los Datos a Establecer
+                if (string.IsNullOrEmpty(value)) throw new Exception("El nombre del trabajador no puede ser null o cadena vacía");
+                
+                // Establecimiento del valor al miembro privadoo
+                _nombre = value;
+
+            }
+            
+        }
+
 
     }
 }
