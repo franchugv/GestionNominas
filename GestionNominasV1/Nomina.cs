6cab3a96e699f9184c29ebacb34b5899f0cab5c7
DAM 1 <DAM 1@DESKTOP-TKRSI9T>
2024-01-16 09:16:43 +0100

    Hemos creado Miembros Privados a la Clase

--- GestionNominasV1/Nomina.cs
+++ GestionNominasV1/Nomina.cs
@@ -8,7 +8,13 @@ namespace GestionNominasV1
 {
     public class Nomina
     {
+        // Miembros privados de la clase
 
+        private string _nombre;
+        private string _apellidos;
+        private string _puesto;
+        private int _horasTrabajadas;
+        private float _salrioHora;
 
 
     }
