0bddd0321b08fa954b7e6fb888713793229da59d
DAM 1 <DAM 1@DESKTOP-TKRSI9T>
2024-01-16 08:53:02 +0100

    Agregar archivos de proyecto.

---
+++ GestionNominasV1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GestionNominasV1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Controlador Principal
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+
+            // CONSTANTES
+
+
+            // RECURSOS
+
+
+            // INICIALIZACION DE RECURSOS
+
+
+            // ENTRADA
+
+
+            // PROCESO
+
+
+            // SALIDA
+
+
+        }
+    }
+}
